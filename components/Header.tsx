772081758bd05f5f2a0fb7eb296c2d307694f542
Helder <helder.morais@vilanova.com.br>
2021-02-28 15:49:54 -0300

    Header

---
+++ components/Header.tsx
@@ -0,0 +1,9 @@
+import styles from './Header.module.css';
+
+export default function Header() {
+    return(
+        <h1 className={styles.title}>
+          <h1>BEM VINDO NEXT JS</h1>
+        </h1>
+    )
+}
